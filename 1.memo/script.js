f03e781dcd2b26c39de576fe71969c5207da6c1a
ignacioLopezDev <ignaciolopez@outlook.com>
2022-08-16 09:36:21 -0300

    3er commit

--- 1.memo/script.js
+++ 1.memo/script.js
@@ -1,11 +1,12 @@
-const MAXIMOS_INTENTOS = 8, // Intentos máximos que tiene el jugador
-    COLUMNAS = 4, // Columnas del memorama
-    SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 1, // Por cuántos segundos mostrar ambas imágenes
-    NOMBRE_IMAGEN_OCULTA = "./img/question.png"; // La imagen que se muestra cuando la real está oculta
+
+const MAXIMOS_INTENTOS = 8, 
+    COLUMNAS = 4, 
+    SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 1, 
+    NOMBRE_IMAGEN_OCULTA = "./img/question.png"; 
 new Vue({
     el: "#app",
     data: () => ({
-        // La ruta de las imágenes. Puede ser relativa o absoluta
+        
         imagenes: [
             "./img/cabra.jpg",
             "./img/conejo.jpg",
@@ -14,4 +15,38 @@ new Vue({
             "./img/perro.jpg",
             "./img/gato.jpg",
         ],
-    })})
+        memorama: [],
+        
+        ultimasCoordenadas: {
+            indiceFila: null,
+            indiceImagen: null,
+        },
+        NOMBRE_IMAGEN_OCULTA: NOMBRE_IMAGEN_OCULTA,
+        MAXIMOS_INTENTOS: MAXIMOS_INTENTOS,
+        intentos: 0,
+        aciertos: 0,
+        esperandoTimeout: false,
+    }),
+    methods: {
+        mostrarCreditos() {
+            Swal.fire({
+                title: "Acerca de",
+                confirmButtonText: "Cerrar",
+                allowOutsideClick: false,
+                allowEscapeKey: false,
+            });
+        },
+    
+        indicarFracaso() {
+            Swal.fire({
+                    title: "Perdiste",
+                    html: `
+                <img class="img-fluid" src="./img/perdiste.png" alt="Perdiste">
+                <p class="h4">Agotaste tus intentos</p>`,
+                    confirmButtonText: "Jugar de nuevo",
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                })
+                .then(this.reiniciarJuego)
+        }
+    }})
